95919d4166378bce60225b7d9e450cd577ab1e00
noahhhhhh <noah.xm.shaw@gmail.com>
2014-10-11 10:52:03 +1100

    minor change on caption

--- plot2.R
+++ plot2.R
@@ -5,7 +5,7 @@ setwd("~/R/Working Directory/ExData_Plotting1")
 source("prep.R")
 
 ##  generate a line chart for Global_active_power against datetime
-plot(data$datetime, data$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)")
+plot(data$datetime, data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
 
 ##  export the plot to a png file and turn off the printing device
 dev.copy(png, file="plot2.png", height=480, width=480)
